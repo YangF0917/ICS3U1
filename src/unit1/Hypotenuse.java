e88f96a36fbd996ac10ed310858414b5ad8b9345
Felix <felix.yang19@ycdsbk12.ca>
2017-10-02 14:03:18 -0400

    Hypotenuse.java

--- src/unit1/Hypotenuse.java
+++ src/unit1/Hypotenuse.java
@@ -1,8 +1,16 @@
 package unit1;
-import codehs.ConsoleProgram;
+import codehs.*;
 
 public class Hypotenuse extends ConsoleProgram{
     public void run(){
-
+        //Create sidea, sideb, hyp variables
+        double sidea, sideb, hyp;
+        //Get length values for side a&b
+        sidea = readDouble("Enter length of Side A: ");
+        sideb = readDouble("Enter length of Side B: ");
+        //Compute hypotenuse
+        hyp = math.sqrt(math.pow(sidea,2)+math.pow(sideb,2));
+        //Output hypotenuse
+        System.out.println("The hypotenuse of the triangle is ")
     }
 }
