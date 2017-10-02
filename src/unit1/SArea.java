e5745f8b5b5a49fb6e243ebb252983c4d5fed972
Felix <felix.yang19@ycdsbk12.ca>
2017-10-02 13:37:48 -0400

    SArea.java program

--- src/unit1/SArea.java
+++ src/unit1/SArea.java
@@ -7,6 +7,6 @@ public class SArea extends ConsoleProgram {
         l = readDouble("What is the length of the box?");
         w = readDouble("What is the width of the box?");
         h = readDouble("What is the height of the box?");
-        System.out.println("The surface area of the box is: "+ 2(l*w+l*h+h*w));
+        System.out.println("The surface area of the box is: "+ 2*(l*w+l*h+h*w));
     }
 }
