cd0a2d13bf24c70a9b7bfbdcde32dc9371c1b95e
Felix <felix.yang19@ycdsbk12.ca>
2017-10-02 14:28:07 -0400

    Hours.java

--- src/unit1/SArea.java
+++ src/unit1/SArea.java
@@ -3,10 +3,13 @@ import codehs.ConsoleProgram;
 
 public class SArea extends ConsoleProgram {
     public void run(){
+        //Create l,w and h variables
         double l, w, h;
+        //Get l, w and h from user input
         l = readDouble("What is the length of the box?");
         w = readDouble("What is the width of the box?");
         h = readDouble("What is the height of the box?");
+        //Compute and Output 2*(l*w+l*h+h*w)
         System.out.println("The surface area of the box is: "+ 2*(l*w+l*h+h*w));
     }
 }
