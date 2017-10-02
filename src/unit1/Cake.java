444d534723c392c6daac8f5acfb8af12ff3329ab
Felix <felix.yang19@ycdsbk12.ca>
2017-10-02 14:25:05 -0400

    Cake.java

--- src/unit1/Cake.java
+++ src/unit1/Cake.java
@@ -3,8 +3,11 @@ import codehs.ConsoleProgram;
 
 public class Cake extends ConsoleProgram{
     public void run (){
+    //Create cake variable
     int cake;
+    //Get cake from user input
     cake = readInt("How many pieces of cake were eaten? ");
+    //Output and compute 2.25*cake
     System.out.println("You need to jog "+ 2.25*cake + " km");
     }
 }
