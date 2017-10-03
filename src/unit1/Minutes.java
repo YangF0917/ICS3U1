3773cae17cb1ce1337b888937f1bbee393627a5c
Felix <felix.yang19@ycdsbk12.ca>
2017-10-03 08:50:40 -0400

    Minutes.java

--- src/unit1/Minutes.java
+++ src/unit1/Minutes.java
@@ -3,8 +3,11 @@ import codehs.*;
 
 public class Minutes extends ConsoleProgram{
     public void run(){
-        
+        //Create minutes variable
         int minutes;
-
+        //Get minutes from user input
+        minutes = readInt("Enter the number of minutes: ");
+        //Compute and output minutes/1440, (minutes%1440)/60, minutes%60
+        System.out.println(minutes + " is equal to "+minutes/1440 + " days, "+ (minutes%1440)/60 + " hours "+ minutes%60+ " minutes.");
     }
 }
