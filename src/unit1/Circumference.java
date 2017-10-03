5b3a1da9647290261fd1e87105bd45dfb66dd713
Felix <felix.yang19@ycdsbk12.ca>
2017-10-03 08:56:58 -0400

    Minutes.java

---
+++ src/unit1/Circumference.java
@@ -0,0 +1,13 @@
+package unit1;
+import codehs.*;
+
+public class Circumference extends ConsoleProgram {
+    public void run(){
+        //Create radius variables
+        double radius;
+        //Get radius from user input
+        radius = readDouble("Enter the radius of the circle: ");
+        //Compute and output 2*radius*Math.PI
+        System.out.println("The circumference of the circle is "+2*radius*Math.PI);
+    }
+}
