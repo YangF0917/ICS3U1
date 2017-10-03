c2613576b1bf8649dfdc1f03ba269a3b357b9363
Felix <felix.yang19@ycdsbk12.ca>
2017-10-03 08:57:29 -0400

    Circumference.java

--- src/unit1/Circumference.java
+++ src/unit1/Circumference.java
@@ -9,5 +9,6 @@ public class Circumference extends ConsoleProgram {
         radius = readDouble("Enter the radius of the circle: ");
         //Compute and output 2*radius*Math.PI
         System.out.println("The circumference of the circle is "+2*radius*Math.PI);
+        
     }
 }
