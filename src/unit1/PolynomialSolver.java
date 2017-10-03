eab14193f0c20393b67de493cecbd22cddbb90ff
Felix <felix.yang19@ycdsbk12.ca>
2017-10-03 13:13:29 -0400

    Poly.java

--- src/unit1/PolynomialSolver.java
+++ src/unit1/PolynomialSolver.java
@@ -1,5 +1,8 @@
 package unit1;
 import codehs.*;
+import com.sun.org.apache.bcel.internal.generic.StackProducer;
+
+import javax.jnlp.IntegrationService;
 
 public class PolynomialSolver extends ConsoleProgram{
     public void run() {
@@ -8,14 +11,16 @@ public class PolynomialSolver extends ConsoleProgram{
         //Get degree from user input before initializing the double array
         degree = readInt("Enter the degree of your polynomial: ");
         //Initialize the polynomial array and answer array
-        double[] poly = new double[degree+1];
+        int[] poly = new int[degree+1];
         double[] ans = new double[degree];
         //Ask for user input for each coefficient
         for (int i = 0; i<degree+1;i++){
-            poly[i] = readDouble("Enter the coefficient of the polynomial at degree: "+i+": ");
+            poly[i] = readInt("Enter the coefficient of the polynomial at degree: "+i+": ");
         }
+        int counter =0;
+        for (int j = 1; j<poly[degree+1];j++){
 
-
-
+        }
+        Stack<Integer>s =new Stack<Integer>();
     }
 }
