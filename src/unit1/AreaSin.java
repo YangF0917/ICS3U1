ff5cc504f2ef294c864bfb7fafa0f7e10bd51a27
Felix <felix.yang19@ycdsbk12.ca>
2017-10-03 09:18:50 -0400

    AreaSin

---
+++ src/unit1/AreaSin.java
@@ -0,0 +1,15 @@
+package unit1;
+import codehs.*;
+
+public class AreaSin extends ConsoleProgram{
+    public void run(){
+        //Create sidea, sideb, and angc variables
+        double sidea, sideb, angc;
+        //Get sidea, sideb, angc variables
+        sidea = readDouble("Enter the first side length: ");
+        sideb = readDouble("Enter the second side length: ");
+        angc = readDouble("Enter the contained angle in radians: ");
+        //Compute and output (sidea*sideb*Math.sin(angc))/2
+        System.out.println("The area of the triangle is "+ (sidea*sideb*Math.sin(angc))/2);
+    }
+}
