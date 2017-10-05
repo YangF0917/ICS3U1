3ffd6ae33024ab2ec27a9312c2ef36f4d09d00fe
Felix <felix.yang19@ycdsbk12.ca>
2017-10-05 12:14:02 -0400

    10-05-2017

---
+++ src/unit1/livehack1/LiveHackProblem1.java
@@ -0,0 +1,8 @@
+package unit1.livehack1;
+import codehs.*;
+
+public class LiveHackProblem1 extends ConsoleProgram{
+    public void run(){
+        
+    }
+}
---
+++ src/unit1/livehack1/LiveHackProblem2.java
@@ -0,0 +1,8 @@
+package unit1.livehack1;
+import codehs.*;
+
+public class LiveHackProblem2 extends ConsoleProgram{
+    public void run(){
+
+    }
+}
