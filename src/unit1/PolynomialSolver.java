efe0d2488e29911f0bd4de421393fa049719e9d9
Felix <felix.yang19@ycdsbk12.ca>
2017-10-04 22:08:47 -0400

    10-04-2017

--- src/unit1/PolynomialSolver.java
+++ src/unit1/PolynomialSolver.java
@@ -3,35 +3,47 @@ import codehs.*;
 
 public class PolynomialSolver extends ConsoleProgram{
     public void run() {
-        //Create degree, coefficients in arrays.
+        // Create degree variable to store the degree of the polynomial
         int degree;
-        //Get degree from user input before initializing the double array
+        // Get degree from user input before initializing the double array
         degree = readInt("Enter the degree of your polynomial: ");
-        //Initialize the polynomial array and answer array
+        // Initialize the polynomial array and answer array
         int[] poly = new int[degree+1];
         double[] ans = new double[degree];
-        //Ask for user input for each coefficient
+        // Ask for user input for each coefficient
         for (int i = 0; i<degree+1;i++){
             poly[i] = readInt("Enter the coefficient of the polynomial at degree: "+i+": ");
         }
-
+        // Creating 2 counter variables to input a number at a certain index and a third variable to
+        // find the index of the first non-zero co-efficient
         int counter = 0;
         int counter2 = 0;
-        for (int j = 1; j<poly[degree]; j++){
-            if (poly[degree]%j == 0){
-                counter++;
-            }
-        }
+        int index = 0;
+        // Storing the factors of the leading co-efficient
         int[] faca = new int[poly[degree]];
         for (int k = 1; k<=poly[degree];k++){
             if (poly[degree]%k == 0){
-                faca[counter2] = k;
-                counter2++;
+                faca[counter] = k;
+                counter++;
             }
-            if ();
+            if (k == poly[degree]){
+                faca[counter] = k;
+            }
         }
+        // Finding the index of the first non-zero value in the array
+        for (int i = 0; i<degree;i++){
+            if (poly[i] != 0){
+                index = i;
+                break;
+            }
+        }
+        // Create an array to hold all the factors of the first non-zero coefficient
+        
         for (int l = 0; l<counter; l++){
             System.out.println(faca[l]);
+        }
+        for (int l = 0; l<counter2; l++){
+            System.out.println(facb[l]);
         }
     }
 }
