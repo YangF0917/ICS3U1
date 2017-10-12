47321cd471822e2a72b0d2132913fd3f18959bb8
Felix <felix.yang19@ycdsbk12.ca>
2017-10-12 12:46:11 -0400

    LiveHackProblem2

--- src/unit1/livehack1/LiveHackProblem2.java
+++ src/unit1/livehack1/LiveHackProblem2.java
@@ -20,7 +20,6 @@ public class LiveHackProblem2 extends ConsoleProgram{
 
         // Compute c = 5.0 / 9 * (f - 32);
         degc = (5.0 / 9) * (degf - 32);
-
         // Output c
         System.out.println("The temperature in Celsius is "+degc+" degrees.");
     }
