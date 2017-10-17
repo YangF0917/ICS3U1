3c8cc19ab517248c0c5c0c87377d6a324bfa3b05
Felix <felix.yang19@ycdsbk12.ca>
2017-10-17 14:20:52 -0400

    Sum5

---
+++ src/unit2/forLoopPractice/Sum5.java
@@ -0,0 +1,17 @@
+package unit2.forLoopPractice;
+import codehs.*;
+import java.util.*;
+
+public class Sum5 extends ConsoleProgram {
+    public void run()
+    {
+        Scanner sc = new Scanner(System.in);
+        int a, b, c, d, e;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        e = sc.nextInt();
+        System.out.println(a+b+c+d+e);
+    }
+}
