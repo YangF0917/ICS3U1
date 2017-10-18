808129ed1aa436052b772599e219c502c63d69c9
Felix <felix.yang19@ycdsbk12.ca>
2017-10-17 21:56:07 -0400

    Avg5

--- src/unit2/forLoopPractice/Avg5.java
+++ src/unit2/forLoopPractice/Avg5.java
@@ -5,13 +5,11 @@ import java.util.*;
 public class Avg5 extends ConsoleProgram {
     public void run()
     {
-        Scanner sc = new Scanner (System.in);
-        int a, b, c, d, e;
-        a = sc.nextInt();
-        b = sc.nextInt();
-        c = sc.nextInt();
-        d = sc.nextInt();
-        e = sc.nextInt();
-        System.out.println((a+b+c+d+e)/5.0);
+        int sum = 0;
+        for (int i = 1; i<=5; i++)
+        {
+            sum += readInt("Enter a number: ");
+        }
+        System.out.println(sum/5.0);
     }
 }
