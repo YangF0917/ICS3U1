e9e4a5f63f87b72600b303b9c7a70ec15439464c
Felix <felix.yang19@ycdsbk12.ca>
2017-10-31 12:30:39 -0400

    10-31-17

--- src/unit2/livehack2/liveHack2Problem2.java
+++ src/unit2/livehack2/liveHack2Problem2.java
@@ -10,6 +10,18 @@ import codehs.*;
 public class liveHack2Problem2 extends ConsoleProgram{
     public void run()
     {
+        // Create a variable distance to equal 800 and a year to be the starting year 2017
+        int distance = 800;
+        int year = 2017;
 
+        // Using a while loop, find out when the distance would be greater than or equal to 1600
+        while (distance < 1600)
+        {
+            distance += 7;
+            year++;
+        }
+
+        // After the while loop has finished changing the value of year, output the variable year
+        System.out.println(year);
     }
 }
