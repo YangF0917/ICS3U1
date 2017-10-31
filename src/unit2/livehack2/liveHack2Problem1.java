50c2bafc3bf77f837425d7a6dd7825ca4a846c48
Felix <felix.yang19@ycdsbk12.ca>
2017-10-31 12:39:18 -0400

    10-31-17

--- src/unit2/livehack2/liveHack2Problem1.java
+++ src/unit2/livehack2/liveHack2Problem1.java
@@ -17,14 +17,15 @@ public class liveHack2Problem1 extends ConsoleProgram{
         // Get year from user input
         year = readInt("Enter the future year: ");
 
-        // Initialize the initial distance to be 800
+        // Initialize the initial distance to be 800 m
         distance = 800;
 
-        // Create a for loop to print out the distance of the glacier every year until less than year
+        // Create a for loop to print out the distance of the glacier every year until less than the year from user input
         for (int i = 2017; i <= year; i++)
         {
-            // Output the year and the meters value
-            distance = 800+(i-2017)*7;
+            // Compute the distance between the lodge and the glacier using 800 + (i - 2017) * 7
+            distance = 800 + (i - 2017) * 7;
+            // Output the year using the value i and the distance using the variable distance
             System.out.println("Year: "+ i + ", Glacier Edge Distance: "+ distance);
         }
     }
