bac2d1986447fe87671bfb1a4988a9f2c048334f
Felix <felix.yang19@ycdsbk12.ca>
2017-10-31 12:52:22 -0400

    10-31-17

--- src/unit2/livehack2/liveHack2Problem1.java
+++ src/unit2/livehack2/liveHack2Problem1.java
@@ -20,13 +20,25 @@ public class liveHack2Problem1 extends ConsoleProgram{
         // Initialize the initial distance to be 800 m
         distance = 800;
 
-        // Create a for loop to print out the distance of the glacier every year until less than the year from user input
-        for (int i = 2017; i <= year; i++)
-        {
-            // Compute the distance between the lodge and the glacier using 800 + (i - 2017) * 7
-            distance = 800 + (i - 2017) * 7;
-            // Output the year using the value i and the distance using the variable distance
-            System.out.println("Year: "+ i + ", Glacier Edge Distance: "+ distance);
+        // Just in case, if the user decides to mess with the year, test whether the year is greater than or equal to 2017
+        if (year >= 2017) {
+            // Create a for loop to print out the distance of the glacier every year until less than the year from user input
+            for (int i = 2017; i <= year; i++) {
+            /*
+            Compute the distance between the lodge and the glacier using 800 + (i - 2017) * 7
+            It is important that the distance value is reinitialized to 800 every time you add distance to it so that
+            the addition does not accumulate.
+            It is possible to find the change in year from 2017 by subtracting the current year 2017 from the year that
+            is being accessed (i) by using (i - 2017)
+            By using distance += (i - 2017) * 7; the addition from the last year would be added to the value stored in
+            distance
+            It is also possible to use distance += 7; but it would not work for values less than 2017
+            */
+                distance = 800 + (i - 2017) * 7;
+
+                // Output the year using the value i and the distance using the variable distance
+                System.out.println("Year: " + i + ", Glacier Edge Distance: " + distance);
+            }
         }
     }
 }
--- src/unit2/livehack2/liveHack2Problem2.java
+++ src/unit2/livehack2/liveHack2Problem2.java
@@ -20,13 +20,14 @@ public class liveHack2Problem2 extends ConsoleProgram{
             /*
             If the distance is less than 1600, then it adds 7 to the distance and 1 to the year
             And then checks again for the next year. The loop stops once it has a year and a distance greater than or
-            equal to 1600 m
+            equal to 1600 m. If the distance is greater than or equal to 1600, then the loop will stop processing
+            this calculation and will stop at the year that corresponds to a distance greater than or equal to 1600
             */
             distance += 7;
             year++;
         }
 
         // After the while loop has finished changing the value of year, output the variable year
-        System.out.println("The lodge will need to close in "+year);
+        System.out.println("The lodge will need to close in " + year);
     }
 }
