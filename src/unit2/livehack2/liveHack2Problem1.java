59fda420f4256cd0dffe596989bb813b08f6b7dc
Felix <felix.yang19@ycdsbk12.ca>
2017-10-31 12:23:08 -0400

    10-31-17

--- src/unit2/livehack2/liveHack2Problem1.java
+++ src/unit2/livehack2/liveHack2Problem1.java
@@ -11,5 +11,21 @@ public class liveHack2Problem1 extends ConsoleProgram{
     public void run()
     {
 
+        // Create 2 variables, one to hold the user input for the year and the other to hold the distance value
+        int distance, year;
+
+        // Get year from user input
+        year = readInt("Enter the future year: ");
+
+        // Initialize the initial distance to be 800
+        distance = 800;
+
+        // Create a for loop to print out the distance of the glacier every year until less than year
+        for (int i = 2017; i <= year; i++)
+        {
+            // Output the year and the meters value
+            distance = 800+(i-2017)*7;
+            System.out.println("Year: "+ i + ", Glacier Edge Distance: "+ distance);
+        }
     }
 }
