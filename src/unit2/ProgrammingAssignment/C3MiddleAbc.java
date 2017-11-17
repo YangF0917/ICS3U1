bebc6df1e36df7287323bc8bb9b622bc26aec51e
Felix <felix.yang19@ycdsbk12.ca>
2017-11-17 13:27:18 -0500

    2017-11-17

--- src/unit2/ProgrammingAssignment/C3MiddleAbc.java
+++ src/unit2/ProgrammingAssignment/C3MiddleAbc.java
@@ -1,4 +1,14 @@
 package unit2.ProgrammingAssignment;
+import codehs.*;
 
-public class C3MiddleAbc {
+public class C3MiddleAbc extends ConsoleProgram{
+    public void run(){
+        // Create a variable to hold the string that the user inputs
+        String userIn;
+
+        // Get user input from the user
+        userIn = readLine("Enter your abc string: \n");
+
+        // 
+    }
 }
--- src/unit2/ProgrammingAssignment/C4IsSandwich.java
+++ src/unit2/ProgrammingAssignment/C4IsSandwich.java
@@ -1,5 +1,17 @@
 package unit2.ProgrammingAssignment;
+import codehs.*;
+import java.util.*;
+import java.lang.*;
 
-public class C4IsSandwich {
+public class C4IsSandwich extends ConsoleProgram{
+    public void run(){
+        // Create a variable to hold the user input
+        String userIn;
+
+        // Get the value of userIn from the user
+        userIn = readLine("Enter your string: \n");
+
+        //
+    }
 
 }
