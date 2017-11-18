7f7fbd3f2b94c4b2bb3f13ef2700583667bbb8fe
Felix <felix.yang19@ycdsbk12.ca>
2017-11-18 13:01:38 -0500

    2017-11-17

--- src/unit2/ProgrammingAssignment/C3MiddleAbc.java
+++ src/unit2/ProgrammingAssignment/C3MiddleAbc.java
@@ -9,6 +9,28 @@ public class C3MiddleAbc extends ConsoleProgram{
         // Get user input from the user
         userIn = readLine("Enter your abc string: \n");
 
-        // 
+        // First check whether the length of the string is perfectly divisible by 2 and do something accordingly
+        if (userIn.length() % 2 == 1)
+        {
+            // Given an example AAabcBB - Index of abc is 2 or AabcB index of abc is 1
+            if (userIn.indexOf("abc") == userIn.length() / 2 - 1)
+            {
+                System.out.println("true");
+            }
+            else{
+                System.out.println("false");
+            }
+        }
+        // The defaulted else is for the even length strings
+        else{
+            if (userIn.indexOf("abc") == userIn.length() / 2 - 1 || userIn.indexOf("abc") == userIn.length() / 2 - 2)
+            {
+                System.out.println("true");
+            }
+            else{
+                System.out.println("false");
+            }
+        }
+
     }
 }
