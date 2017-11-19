1a09a5498ba1e760e905a4b4d4451745d1479439
Felix <felix.yang19@ycdsbk12.ca>
2017-11-18 21:06:13 -0500

    2017-11-18

--- src/unit2/ProgrammingAssignment/C3MiddleAbc.java
+++ src/unit2/ProgrammingAssignment/C3MiddleAbc.java
@@ -22,29 +22,28 @@ public class C3MiddleAbc extends ConsoleProgram{
         boolean isAbcMiddle = false;
 
         // First check whether the length of the string is perfectly divisible by 2 and do something accordingly
-        if (userIn.length() % 2 == 1)
-        {
+        if (!userIn.equals("")) {
+            if (userIn.length() % 2 == 1) {
             /*
             Knowing that the string is has an odd length, the leter b in abc would be the direct middle of the string
             if abc is in the middle
              */
-            if (userIn.substring(userIn.length() / 2 - 1, userIn.length() / 2 + 2).equals("abc"))
-            {
-                isAbcMiddle = true;
+                if (userIn.substring(userIn.length() / 2 - 1, userIn.length() / 2 + 2).equals("abc")) {
+                    isAbcMiddle = true;
+                }
             }
-        }
-        // The defaulted else is for the even length strings
-        else{
+            // The defaulted else is for the even length strings
+            else {
             /*
             Find out whether the index of abc in a string would one less than half or two less than half. Ex.
             AabcBB, the index of abc is 1, the string length is 6, so the result would be length / 2 - 2
             AAabcB the index of abc is 2, the string length is 6, so the result would be length / 2 - 1
             These are the only two cases for even numbers therefore if either are correct, then abc is in the middle
              */
-            if (userIn.substring(userIn.length() / 2 - 1, userIn.length() / 2 + 2).equals("abc") || userIn.substring
-                    (userIn.length() / 2 - 2, userIn.length() / 2 + 1).equals("abc"))
-            {
-                isAbcMiddle = true;
+                if (userIn.substring(userIn.length() / 2 - 1, userIn.length() / 2 + 2).equals("abc") || userIn.substring
+                        (userIn.length() / 2 - 2, userIn.length() / 2 + 1).equals("abc")) {
+                    isAbcMiddle = true;
+                }
             }
         }
         System.out.println(isAbcMiddle);
