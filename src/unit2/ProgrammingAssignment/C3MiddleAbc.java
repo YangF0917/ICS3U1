0ee73745b260d89819c7f8340f2b23d0b833173a
Felix <felix.yang19@ycdsbk12.ca>
2017-11-18 20:19:57 -0500

    2017-11-18

--- src/unit2/ProgrammingAssignment/C3MiddleAbc.java
+++ src/unit2/ProgrammingAssignment/C3MiddleAbc.java
@@ -12,7 +12,10 @@ public class C3MiddleAbc extends ConsoleProgram{
         // First check whether the length of the string is perfectly divisible by 2 and do something accordingly
         if (userIn.length() % 2 == 1)
         {
-            // Given an example AAabcBB - Index of abc is 2 or AabcB index of abc is 1
+            /*
+            Knowing that the string is has an odd length, the leter b in abc would be the direct middle of the string
+            if abc is in the middle
+             */
             if (userIn.indexOf("abc") == userIn.length() / 2 - 1)
             {
                 System.out.println("true");
@@ -23,6 +26,12 @@ public class C3MiddleAbc extends ConsoleProgram{
         }
         // The defaulted else is for the even length strings
         else{
+            /*
+            Find out whether the index of abc in a string would one less than half or two less than half. Ex.
+            AabcBB, the index of abc is 1, the string length is 6, so the result would be length / 2 - 2
+            AAabcB the index of abc is 2, the string length is 6, so the result would be length / 2 - 1
+            These are the only two cases for even numbers therefore if either are correct, then abc is in the middle
+             */
             if (userIn.indexOf("abc") == userIn.length() / 2 - 1 || userIn.indexOf("abc") == userIn.length() / 2 - 2)
             {
                 System.out.println("true");
--- src/unit2/ProgrammingAssignment/C4IsSandwich.java
+++ src/unit2/ProgrammingAssignment/C4IsSandwich.java
@@ -11,7 +11,11 @@ public class C4IsSandwich extends ConsoleProgram{
         // Get the value of userIn from the user
         userIn = readLine("Enter your string: \n");
 
-        //
+        // Find the first instance of bread in the string
+        int firstBread = userIn.indexOf("bread");
+
+        // Create a value to hold the last index of bread that was found using multiple substrings
+        
     }
 
 }
