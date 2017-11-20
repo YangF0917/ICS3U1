c46a6e1c912937369961b7bf1151b62c53e9d3c6
Felix <felix.yang19@ycdsbk12.ca>
2017-11-20 08:58:47 -0500

    2017-11-20

--- src/unit2/ProgrammingAssignment/C4IsSandwich.java
+++ src/unit2/ProgrammingAssignment/C4IsSandwich.java
@@ -26,24 +26,28 @@ public class C4IsSandwich extends ConsoleProgram{
         int substringIndex = 0;
 
         // Create a variable to hold the last index of bread that was found
-        int lastBread;
+        int lastBread = -1;
 
         // Find the last value of bread using a do while loop
+        // Create a blank string to hold the cut original string
+        String userSubstring = "";
         do {
-            // Create a blank string to hold the cut original string
-            String userSubstring = "";
             /*
              Essentially, the original string is being cut one letter at a time from the front and if it finds the word
              bread anywhere in the substring, it stores the value of the index of bread in the lastBread variable
               */
             userSubstring = userIn.substring(substringIndex);
-            lastBread = userSubstring.indexOf("bread");
+            System.out.println(userSubstring);
+            if (userSubstring.indexOf("bread")!= -1) {
+                lastBread = userSubstring.indexOf("bread");
+                lastBread += substringIndex;
+            }
             substringIndex++;
         }
         while(substringIndex < userIn.length());
 
         // Printing out the substring in between the two instances of bread
         System.out.println(firstBread + " ," + lastBread);
+        System.out.println(userIn.substring(firstBread + 5, lastBread));
     }
-
 }
