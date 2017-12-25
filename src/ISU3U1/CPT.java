e1c3db499f91ef9ede61dbe1fe931f546a2d720b
Felix <felix.yang19@ycdsbk12.ca>
2017-12-25 09:20:28 -0500

    2017-12-25

---
+++ src/ISU3U1/CPT.java
@@ -0,0 +1,17 @@
+package ISU3U1;
+
+public class CPT {
+
+    public static int passcode;
+    public static int keys = 0;
+    public static String [] places = {"Home", "Abandoned Warehouse", "Water Pump", "Library", "Research Lab"};
+    public static int hydrationWatch = 1;
+
+    public static void main (String[] args){
+
+    }
+    public static void introduction(){
+
+    }
+    
+}
