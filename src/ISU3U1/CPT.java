4037de915208f5a3e37e609b9e15f3717f577aa9
Felix <felix.yang19@ycdsbk12.ca>
2017-12-25 09:25:44 -0500

    2017-12-25

--- src/ISU3U1/CPT.java
+++ src/ISU3U1/CPT.java
@@ -8,10 +8,13 @@ public class CPT {
     public static int hydrationWatch = 1;
 
     public static void main (String[] args){
-
+        introduction();
     }
     public static void introduction(){
-
+        System.out.println("You open your eyes and find yourself on your bed. It's awfully quiet around the house. " +
+                "You find a note on the door that says \"Evacuation Site: Go towar---------------\"" +
+                ". The rest of the ink has been smudged by some red liquid. It's been 2 years since the beginning of" +
+                " the plague and no cure has been found yet. The end of humanity is near.");
     }
-    
+
 }
